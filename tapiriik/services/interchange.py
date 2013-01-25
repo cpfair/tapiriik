ed41ccf12280fc143bbbb848d926477c5284e017
Collin Fair <cpf@cpfx.ca>
2013-01-25 13:10:09 -0500

    Initial interchange architecture

---
+++ tapiriik/services/interchange.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+
+class Activity:
+    def __init__(self, startTime=datetime.min, endTime=datetime.min, waypointList=[]):
+        self.StartTime = startTime
+        self.EndTime = endTime
+        self.Waypoints = waypointList
+
+
+class UploadedActivity (Activity):
+    pass  # will contain list of which service instances contain this activity
+
+
+class Waypoint:
+    def __init__(self, timestamp=None, location=None, hr=None, power=None):
+        self.Timestamp = timestamp or datetime.min
+        self.Location = location
+        self.HR = hr
+        self.Power = power  # I doubt there will ever be more parameters than this in terms of interchange
+
+
+class Location:
+    def __init__(self, lat, lon):
+        self.Latitude = lat
+        self.Longitude = lon
