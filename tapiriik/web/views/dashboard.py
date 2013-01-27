d559b66b1d12ed3feb4f7a12c167d1bdf08ce309
cpf <cpf@cpfx.ca>
2013-01-26 21:39:50 -0500

    Dashboard improvements

--- tapiriik/web/views/dashboard.py
+++ tapiriik/web/views/dashboard.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def dashboard(req):
+    if req.user is not None:
+        req.user["ConnectedServiceIDs"]=[x["Service"] for x in req.user["ConnectedServices"]] if "ConnectedServices" in req.user else []
     return render(req,"dashboard.html",{"user": req.user})
