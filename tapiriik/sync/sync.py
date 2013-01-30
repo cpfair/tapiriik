305c38e4f1c994f420b23a146eaf0df5c5f104cc
Collin Fair <cpf@cpfx.ca>
2013-01-30 13:13:19 -0500

    Refactored for ease of testing

--- tapiriik/sync/sync.py
+++ tapiriik/sync/sync.py
@@ -7,6 +7,20 @@ class Sync:
     def ScheduleImmediateSync(user):
         db.users.update({"_id": user["_id"]}, {"$set": {"NextSynchronization": datetime.utcnow()}})
 
+    def _determineRecipientServices(activity, allConnections):
+        recipientServices = allConnections
+        recipientServices = [conn for conn in recipientServices if activity.Type in Service.FromID(conn["Service"]).SupportedActivities]
+        recipientServices = [conn for conn in recipientServices if "SynchronizedActivities" not in conn or activity.UID not in conn["SynchronizedActivities"]]
+        return recipientServices
+
+    def _accumulateActivitiesToSync(svc, svcActivities, activityList):
+        for act in svcActivities:
+                existElsewhere = [x for x in activityList if x.UID == act.UID]
+                if len(existElsewhere) > 0:
+                    existElsewhere[0].UploadedTo += act.UploadedTo
+                    continue
+                activityList.append(act)
+
     def PerformUserSync(user):
         connectedServiceIds = [x["ID"] for x in user["ConnectedServices"]]
         serviceConnections = list(db.connections.find({"_id": {"$in": connectedServiceIds}}))
@@ -15,25 +29,18 @@ class Sync:
         for conn in serviceConnections:
             svc = Service.FromID(conn["Service"])
             svcActivities = svc.DownloadActivityList(conn)
-
-            for act in svcActivities:
-                existElsewhere = [x for x in activities if x.UID == act.UID]
-                if len(existElsewhere) > 0:
-                    existElsewhere[0].UploadedTo += act.UploadedTo
-                    continue
-                activities.append(act)
+            Sync._accumulateActivitiesToSync(svc, svcActivities, activities)
 
         for activity in activities:
             print (str(activity) + " from " + activity.UploadedTo[0]["Connection"]["Service"] + " ct " + str(len(activity.UploadedTo)))
+
         for activity in activities:
             # we won't need this now, but maybe later
             db.connections.update({"_id": {"$in": [x["Connection"]["_id"] for x in activity.UploadedTo]}},\
                 {"$addToSet": {"SynchronizedActivities": activity.UID}},\
                 multi=True)
-            # python really needs LINQ
-            recipientServices = serviceConnections
-            recipientServices = [conn for conn in recipientServices if activity.Type in Service.FromID(conn["Service"]).SupportedActivities]
-            recipientServices = [conn for conn in recipientServices if "SynchronizedActivities" not in conn or activity.UID not in conn["SynchronizedActivities"]]
+
+            recipientServices = Sync._determineRecipientServices(activity, serviceConnections)
 
             if len(recipientServices) == 0:
                 continue
