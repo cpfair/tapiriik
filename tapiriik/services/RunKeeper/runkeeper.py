495db93a46dae7004fa9454a962196f0c95b3dc6
cpf <cpf@cpfx.ca>
2013-02-11 22:15:46 -0500

    Switch RK client to requests library from httplib2

--- tapiriik/services/RunKeeper/runkeeper.py
+++ tapiriik/services/RunKeeper/runkeeper.py
@@ -5,7 +5,7 @@ from tapiriik.services.interchange import UploadedActivity, ActivityType, Waypoi
 from tapiriik.database import db
 from django.core.urlresolvers import reverse
 from datetime import datetime, timedelta
-import httplib2
+import requests
 import urllib.parse
 import json
 
@@ -46,15 +46,14 @@ class RunKeeperService():
     def RetrieveAuthenticationToken(self, req):
         from tapiriik.services import Service
 
-        wc = httplib2.Http()
         #  might consider a real OAuth client
         code = req.GET.get("code")
         params = {"grant_type": "authorization_code", "code": code, "client_id": RUNKEEPER_CLIENT_ID, "client_secret": RUNKEEPER_CLIENT_SECRET, "redirect_uri": WEB_ROOT + reverse("oauth_return", kwargs={"service": "runkeeper"})}
-        #return urllib.parse.urlencode(params)
-        resp, data = wc.request("https://runkeeper.com/apps/token", method="POST", body=urllib.parse.urlencode(params), headers={"Content-Type": "application/x-www-form-urlencoded"})
-        if resp.status != 200:
+
+        response = requests.post("https://runkeeper.com/apps/token", data=urllib.parse.urlencode(params), headers={"Content-Type": "application/x-www-form-urlencoded"})
+        if response.status_code != 200:
             raise APIAuthorizationException("Invalid code", None)
-        token = json.loads(data.decode('utf-8'))["access_token"]
+        token = response.json()["access_token"]
 
         # hacky, but also totally their fault for not giving the user id in the token req
         existingRecord = Service.GetServiceRecordWithAuthDetails(self, {"Token": token})
@@ -69,12 +68,17 @@ class RunKeeperService():
         return {"Authorization": "Bearer " + serviceRecord["Authorization"]["Token"]}
 
     def _getAPIUris(self, serviceRecord):
-        if hasattr(self, "_uris"):  # cache these for the life of the batch job at least
+        if hasattr(self, "_uris"):  # cache these for the life of the batch job at least? hope so
             return self._uris
         else:
-            wc = httplib2.Http()
-            resp, data = wc.request("https://api.runkeeper.com/user/", headers=self._apiHeaders(serviceRecord))
-            uris = json.loads(data.decode('utf-8'))
+            response = requests.get("https://api.runkeeper.com/user/", headers=self._apiHeaders(serviceRecord))
+
+            if response.status_code != 200:
+                if response.status_code == 401:
+                    raise APIAuthorizationException("No authorization to retrieve user URLs", serviceRecord)
+                raise APIException("Unable to retrieve user URLs" + str(response), serviceRecord)
+
+            uris = response.json()
             for k in uris.keys():
                 if type(uris[k]) == str:
                     uris[k] = "https://api.runkeeper.com" + uris[k]
@@ -82,20 +86,19 @@ class RunKeeperService():
             return uris
 
     def _getUserId(self, serviceRecord):
-        wc = httplib2.Http()
-        resp, data = wc.request("https://api.runkeeper.com/user/", headers=self._apiHeaders(serviceRecord))
-        data = json.loads(data.decode('utf-8'))
+        resp = requests.get("https://api.runkeeper.com/user/", headers=self._apiHeaders(serviceRecord))
+        data = resp.json()
         return data["userID"]
 
     def DownloadActivityList(self, serviceRecord):
         uris = self._getAPIUris(serviceRecord)
-        wc = httplib2.Http()
-        resp, data = wc.request(uris["fitness_activities"], headers=self._apiHeaders(serviceRecord))
-        if int(resp["status"]) != 200:
-            if int(resp["status"]) == 401:
+        response = requests.get(uris["fitness_activities"], headers=self._apiHeaders(serviceRecord))
+
+        if response.status_code != 200:
+            if response.status_code == 401:
                 raise APIAuthorizationException("No authorization to retrieve activity list", serviceRecord)
-            raise APIException("Unable to retrieve activity list " + str(resp), serviceRecord)
-        data = json.loads(data.decode('utf-8'))
+            raise APIException("Unable to retrieve activity list " + str(response), serviceRecord)
+        data = response.json()
         activities = []
         for act in data["items"]:
             activity = self._populateActivity(act)
@@ -118,13 +121,12 @@ class RunKeeperService():
         activityID = [x["ActivityID"] for x in activity.UploadedTo if x["Connection"] == serviceRecord][0]
         ridedata = db.rk_activity_cache.find_one({"uri": activityID})
         if ridedata is None:
-            wc = httplib2.Http()
-            resp, ridedata = wc.request("https://api.runkeeper.com" + activityID, headers=self._apiHeaders(serviceRecord))
-            if int(resp["status"]) != 200:
-                if int(resp["status"]) == 401:
+            response = requests.get("https://api.runkeeper.com" + activityID, headers=self._apiHeaders(serviceRecord))
+            if response.status_code != 200:
+                if response.status_code == 401:
                     raise APIAuthorizationException("No authorization to download activity" + activityID, serviceRecord)
                 raise APIException("Unable to download activity " + activityID, serviceRecord)
-            ridedata = json.loads(ridedata.decode('utf-8'))
+            ridedata = response.json()
             db.rk_activity_cache.insert(ridedata)
 
         self._populateActivityWaypoints(ridedata, activity)
@@ -158,12 +160,12 @@ class RunKeeperService():
         #  assembly dict to post to RK
         uploadData = self._createUploadData(activity)
         uris = self._getAPIUris(serviceRecord)
-        wc = httplib2.Http()
         headers = self._apiHeaders(serviceRecord)
         headers["Content-Type"] = "application/vnd.com.runkeeper.NewFitnessActivity+json"
-        resp, data = wc.request(uris["fitness_activities"], "POST", headers=headers, body=json.dumps(uploadData))
-        if int(resp["status"]) != 201:
-            if int(resp["status"]) == 401:
+        response = requests.post(uris["fitness_activities"], headers=headers, data=json.dumps(uploadData))
+
+        if response.status_code != 201:
+            if response.status_code == 401:
                 raise APIAuthorizationException("No authorization to upload activity " + activity.UID, serviceRecord)
             raise APIException("Unable to upload activity " + activity.UID, serviceRecord)
 
