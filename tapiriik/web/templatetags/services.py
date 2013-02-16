a31b40ba1a719a9a8a98991a2e78e257430f7fc7
cpf <cpf@cpfx.ca>
2013-02-15 19:30:56 -0500

    Refactored dashboard generation + filters

--- tapiriik/web/templatetags/services.py
+++ tapiriik/web/templatetags/services.py
@@ -1,5 +1,6 @@
 from django import template
 from tapiriik.services import Service
+from tapiriik.database import db
 register = template.Library()
 
 
@@ -8,11 +9,21 @@ def IDs(value):
     return [x["Service"] for x in value]
 
 
-@register.filter(name="svc_except")
+@register.filter(name="svc_providers_except")
 def exceptSvc(value):
-    return [x for x in Service.List() if x not in value]
+    return [x for x in Service.List() if x not in providers(value)]
 
 
-@register.filter(name="svc_providers")
+@register.filter(name="svc_conn_to_provider")
+def provider(value):
+    return Service.FromID(value["Service"])
+
+
+@register.filter(name="svc_conns_to_providers")
 def providers(value):
-    return [Service.FromID(x["Service"]) for x in value]
+    return [provider(x) for x in value]
+
+
+@register.filter(name="svc_populate_conns")
+def fullRecords(conns):
+    return db.connections.find({"_id": {"$in": [x["ID"] for x in conns]}})
