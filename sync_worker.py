971402afcba2f180cc83a20fdc37fb923a4e1646
cpf <cpf@cpfx.ca>
2013-02-16 06:16:41 -0500

    Auto-expire sync locks when things go wrong (re. last commit) or crash

--- sync_worker.py
+++ sync_worker.py
@@ -1,7 +1,13 @@
 from tapiriik.sync import Sync
+from tapiriik.database import db
 import time
 import datetime
-print("Sync worker starting at " + datetime.datetime.now().ctime())
+import os
+
+print("Sync worker starting at " + datetime.datetime.now().ctime() + " pid " + str(os.getpid()))
 while True:
     Sync.PerformGlobalSync()
     time.sleep(5)
+    db.users.update({"$or": [{"SynchronizationWorker": os.getpid()},
+                            {"LastSynchronization": {"$lt": datetime.datetime.utcnow() - datetime.timedelta(minutes=30)}}]},  # auto-release after 30 minutes
+                            {"$unset": {"SynchronizationWorker": None}})
