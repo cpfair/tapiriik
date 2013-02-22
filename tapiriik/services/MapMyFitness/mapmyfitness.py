d7b89c77cd04cedeab41676edd2b20564651369e
cpf <cpf@cpfx.ca>
2013-02-21 20:26:46 -0500

    MMF Activity mapping, DownloadActivityList

--- tapiriik/services/MapMyFitness/mapmyfitness.py
+++ tapiriik/services/MapMyFitness/mapmyfitness.py
@@ -1,7 +1,9 @@
 from tapiriik.services.service_authentication import ServiceAuthenticationType
 from tapiriik.services.api import APIException, APIAuthorizationException
+from tapiriik.services.interchange import UploadedActivity, ActivityType, WaypointType, Waypoint, Location
 from tapiriik.settings import WEB_ROOT, MAPMYFITNESS_CLIENT_KEY, MAPMYFITNESS_CLIENT_SECRET
 
+from datetime import datetime, timedelta
 import requests
 from django.core.urlresolvers import reverse
 from requests_oauthlib import OAuth1
@@ -13,6 +15,21 @@ class MapMyFitnessService():
     AuthenticationType = ServiceAuthenticationType.OAuthSigned
     UserAuthorizationURL = None
     OutstandingOAuthRequestTokens = {}
+
+    _activityMappings = {16: ActivityType.Running,
+                         11: ActivityType.Cycling,
+                         41: ActivityType.MountainBiking,
+                         9: ActivityType.Walking,
+                         24: ActivityType.Hiking,
+                         398: ActivityType.DownhillSkiing,
+                         397: ActivityType.CrossCountrySkiing,  # actually "backcountry" :S
+                         107: ActivityType.Snowboarding,
+                         86: ActivityType.Skating,  # ice skating
+                         15: ActivityType.Swimming,
+                         57: ActivityType.Rowing,  # canoe/rowing
+                         211: ActivityType.Elliptical,
+                         21: ActivityType.Other}
+    SupportedActivities = list(_activityMappings.values())
 
     def WebInit(self):
         pass
@@ -75,3 +92,54 @@ class MapMyFitnessService():
         resp = requests.post("http://api.mapmyfitness.com/3.1/oauth/revoke", auth=oauth)
         if resp.status_code != 200:
             raise APIException("Unable to deauthorize MMF auth token, status " + str(resp.status_code) + " resp " + resp.text, serviceRecord)
+
+    def _getActivityTypeHierarchy(self):
+        if hasattr(self, "_activityTypes"):
+            return self._activityTypes
+        response = requests.get("http://api.mapmyfitness.com/3.1/workouts/get_activity_types")
+        data = response.json()
+        self._activityTypes = {}
+        for actType in data["result"]["output"]["activity_types"]:
+            self._activityTypes[int(actType["activity_type_id"])] = actType
+        return self._activityTypes
+
+    def _resolveActivityType(self, actType):
+        self._getActivityTypeHierarchy()
+        while actType not in self._activityMappings or self._activityTypes[actType]["parent_activity_type_id"] is not None:
+            actType = int(self._activityTypes[actType]["parent_activity_type_id"])
+        if actType in self._activityMappings:
+            return self._activityMappings[actType]
+        else:
+            return ActivityType.Other
+
+    def DownloadActivityList(self, serviceRecord, exhaustive=False):
+        oauth = self._getOauthClient(serviceRecord)
+
+        allItems = []
+
+        offset = 0
+
+        while True:
+            response = requests.get("http://api.mapmyfitness.com/3.1/workouts/get_workouts?limit=25&start_record=" + str(offset), auth=oauth)
+            if response.status_code != 200:
+                if response.status_code == 401 or response.status_code == 403:
+                    raise APIAuthorizationException("No authorization to retrieve activity list", serviceRecord)
+                raise APIException("Unable to retrieve activity list " + str(response), serviceRecord)
+            data = response.json()
+            allItems += data["result"]["output"]["workouts"]
+            if not exhaustive or int(data["result"]["output"]["count"]) < 25:
+                break
+
+        activities = []
+        for act in allItems:
+            activity = UploadedActivity()
+            activity.StartTime = datetime.strptime(act["workout_date"] + " " + act["workout_start_time"], "%Y-%m-%d %H:%M:%S")
+            activity.EndTime = activity.StartTime + timedelta(0, round(float(act["time_taken"])))
+            activity.Distance = act["distance"]
+
+            activity.Type = self._resolveActivityType(int(act["activity_type_id"]))
+            activity.CalculateUID()
+
+            activity.UploadedTo = [{"Connection": serviceRecord, "ActivityID": act["workout_id"]}]
+            activities.append(activity)
+        return activities
