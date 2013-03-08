214e390224b78248c62a7ca98199368ffc7d17c4
cpf <cpf@cpfx.ca>
2013-03-07 19:29:20 -0500

    Kick off a sync immediately on payment

--- tapiriik/auth/__init__.py
+++ tapiriik/auth/__init__.py
@@ -28,6 +28,7 @@ class User:
     def AssociatePayment(user, payment):
         db.users.update({"_id": {'$ne': ObjectId(user["_id"])}}, {"$pull": {"Payments": payment}}, multi=True)  # deassociate payment ids from other accounts that may be using them
         db.users.update({"_id": ObjectId(user["_id"])}, {"$addToSet": {"Payments": payment}})
+        Sync.ScheduleImmediateSync(user)
 
     def HasActivePayment(user):
         if "Payments" not in user:
