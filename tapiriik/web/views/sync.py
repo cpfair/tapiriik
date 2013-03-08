9283224b3d9e34cec0d0170d1721e31b8e9ec110
cpf <cpf@cpfx.ca>
2013-03-07 19:04:03 -0500

    Trap None-but-still-set NextSync timestamps

--- tapiriik/web/views/sync.py
+++ tapiriik/web/views/sync.py
@@ -15,8 +15,8 @@ def sync_status(req):
             continue
         totalErrors += len(conn["SyncErrors"])
     print(req.user)
-    return HttpResponse(json.dumps({"NextSync": (req.user["NextSynchronization"].ctime() + " UTC") if "NextSynchronization" in req.user else None,
-                                    "LastSync": (req.user["LastSynchronization"].ctime() + " UTC") if "LastSynchronization" in req.user else None,
+    return HttpResponse(json.dumps({"NextSync": (req.user["NextSynchronization"].ctime() + " UTC") if "NextSynchronization" in req.user and req.user["NextSynchronization"] is not None else None,
+                                    "LastSync": (req.user["LastSynchronization"].ctime() + " UTC") if "LastSynchronization" in req.user and req.user["LastSynchronization"] is not None else None,
                                     "Synchronizing": "SynchronizationWorker" in req.user,
                                     "Errors": totalErrors}), mimetype="application/json")
 
