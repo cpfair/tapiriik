a9be6d06ff6ba5003a7e8dd4b5908604880e03e2
Collin Fair <cpf@cpfx.ca>
2013-03-12 11:13:57 -0400

    untested Dropbox DownloadActivityList, still needs activity type tagging logic

--- tapiriik/services/Dropbox/dropbox.py
+++ tapiriik/services/Dropbox/dropbox.py
@@ -1,8 +1,11 @@
 from tapiriik.settings import WEB_ROOT, DROPBOX_APP_KEY, DROPBOX_APP_SECRET
 from tapiriik.services.service_authentication import ServiceAuthenticationType
+from tapiriik.services.gpx import GPXIO
+from tapiriik.database import cachedb
 from dropbox import client, rest, session
 from django.core.urlresolvers import reverse
-
+from bson.binary import Binary
+import zlib
 
 class DropboxService():
     ID = "dropbox"
@@ -46,3 +49,52 @@ class DropboxService():
         else:
             uid = existingRecord["ExternalID"]
         return (uid, {"Key": accessToken.key, "Secret": accessToken.secret})
+    def _folderRecurse(self, structCache, dbcl, path):
+        hash = None
+        if path in structCache["Directories"]:
+            hash = structCache["Directories"][path]["Hash"]
+        try:
+            dirmetadata = dbcl.metadata(path, hash=hash)
+        except rest.ErrorResponse as e:
+            if e.state == 304:
+                return structCache  # nothing new to update here
+            raise  # an actual issue
+
+        structCache["Directories"][path]={"Hash": dirmetadata["hash"]}
+        for file in dirmetadata["contents"]:
+            if file["is_dir"]:
+                structCache = self._folderRecurse(structCache, dbcl, file["path"])
+            else:
+                if not file["path"].lower().endswith(".gpx"):
+                    continue  # another kind of file
+                structCache["Files"][file["path"]]={"Rev": file["rev"]}
+        return structCache
+    def DownloadActivityList(self, svcRec):
+        dbcl = self._getClient(svcRec)
+        syncRoot = svcRec["Config"]["SyncRoot"]
+        cache = dbcache.dropbox_cache.find_one({"ExternalID": svcRec["ExternalID"]});
+        if cache is None:
+            cache = {"ExternalID": svcRec["ExternalID"], "Structure":{"Files": {}, "Directories": {}}, "Activities":{}}
+        cache["Structure"] = self._folderRecurse(cache["Structure"], dbcl, syncRoot)
+
+        activities = []
+
+        for path, file in cache["Structure"]["Files"]:
+            existUID, existing = [k, x for k, x in cache["Activities"] if x["Path"] == path.replace(syncRoot,"")]  # path is relative to syncroot to reduce churn if they relocate it
+            existing = existing[0] if existing else None
+            if existing and existing["Rev"] == file["Rev"]:
+                data = zlib.decompress(cachedb.dropbox_data_cache.find_one({"UID": existUID})["Data"])  # really need compression?
+                act = GPXIO.Parse(data)
+            else:
+                # get the activity and store the data locally
+                f, metadata = dbcl.get_file_and_metadata(path)
+                data = f.read()
+                act = GPXIO.Parse(data)
+                cache["Activities"][act.UID] = {"Rev": metadata["rev"], "Path": path.replace(syncRoot,"")}
+                cachedb.dropbox_data_cache.update({"UID": act.UID}, {"UID": act.UID, "Data": Binary(zlib.compress(data))}, upsert=True)  # easier than GridFS
+            ## activity tagging stuff here ##
+            activities.append(act)
+
+        cachedb.dropbox_cache.update({"ExternalID": svcRec["ExternalID"]}, cache, upsert=True)
+        return activities
+
