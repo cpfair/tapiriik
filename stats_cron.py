a279ee27f3f68eadf168d4230ff06258b9f84587
Collin Fair <cpf@cpfx.ca>
2013-05-24 00:13:33 -0400

    Fixed incorrect sync-time utilization calculation that would include records outside of the sliding window

--- stats_cron.py
+++ stats_cron.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 distanceSynced = db.sync_stats.aggregate([{"$group": {"_id": None, "total": {"$sum": "$Distance"}}}])["result"][0]["total"]
 
 #sync time utilization
-timeUsed = db.sync_worker_stats.aggregate([{"$group": {"_id": None, "total": {"$sum": "$TimeTaken"}}}])["result"][0]["total"]
 db.sync_worker_stats.remove({"Timestamp": {"$lt": datetime.utcnow() - timedelta(hours=1)}})  # clean up old records
+timeUsed = db.sync_worker_stats.aggregate([{"$group": {"_id": None, "total": {"$sum": "$TimeTaken"}}}])["result"][0]["total"]
 
 db.stats.update({}, {"$set": {"TotalDistanceSynced": distanceSynced, "TotalSyncTimeUsed": timeUsed, "Updated": datetime.utcnow()}}, upsert=True)
