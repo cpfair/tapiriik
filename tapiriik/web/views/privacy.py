898b71d120aa6df09e23e3c430d0bff2f36153c0
Collin Fair <cpf@cpfx.ca>
2013-11-27 16:43:07 -0500

    Add RWGPS to privacy page.

--- tapiriik/web/views/privacy.py
+++ tapiriik/web/views/privacy.py
@@ -16,6 +16,7 @@ def privacy(request):
 	services["sporttracks"].update({"email": OPTIN, "password": OPTIN, "tokens": NO, "metadata": YES, "data":NO})
 	services["dropbox"].update({"email": NO, "password": NO, "tokens": YES, "metadata": YES, "data":CACHED})
 	services["runkeeper"].update({"email": NO, "password": NO, "tokens": YES, "metadata": YES, "data":NO})
+	services["rwgps"].update({"email": OPTIN, "password": OPTIN, "tokens": NO, "metadata": YES, "data":NO})
 
 	services_list = [x for key, x in services.items()]
 	return render(request, "privacy.html", {"services": services_list})
