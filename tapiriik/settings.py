d195971bab68640a0b4eb4ffc70ab9bf91e75613
Collin Fair <cpf@cpfx.ca>
2013-12-21 17:06:49 -0700

    Removed all old Endomondo code, un-withdrew it.

--- tapiriik/settings.py
+++ tapiriik/settings.py
@@ -182,7 +182,7 @@ SOFT_LAUNCH_SERVICES = []
 DISABLED_SERVICES = []
 
 # Services no longer available - will be removed across the site + excluded from sync.
-WITHDRAWN_SERVICES = ["endomondo"]
+WITHDRAWN_SERVICES = []
 
 # Where to put per-user sync logs
 USER_SYNC_LOGS = "./"
