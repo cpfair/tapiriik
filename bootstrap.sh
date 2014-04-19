9f118b8ab391600b2a99b63e78d6ddcd05b82fcb
Collin Fair <collin@getpebble.com>
2014-04-18 18:33:20 -0700

    All the cool kids are doing it.

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install system requirements
+sudo apt-get update
+sudo apt-get install -y python3-pip libxml2-dev libxslt-dev zlib1g-dev mongodb git
+
+# Fix pip
+pip3 install --upgrade pip
+
+# Install app requirements
+pip install -r /vagrant/requirements.txt
+
+# Fix the default python instance
+sudo rm `which python`
+sudo ln -s /usr/bin/python3.3 /usr/bin/python
+
+# Put in a default local_settings.py
+cp /vagrant/tapiriik/local_settings.py.example /vagrant/tapiriik/local_settings.py
