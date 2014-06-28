f657f177a0edbff98b88f232e7eb31770c1a3006
Collin Fair <cpf@cpfx.ca>
2014-06-27 22:14:41 -0400

    I don't understand why this can't be automatic.

--- sync_poll_triggers.py
+++ sync_poll_triggers.py
@@ -10,6 +10,7 @@ if isinstance(settings.HTTP_SOURCE_ADDR, list):
 
 from tapiriik.services import Service
 from celery import Celery
+from celery.signals import worker_shutdown
 from datetime import datetime
 
 class _celeryConfig:
@@ -20,6 +21,10 @@ class _celeryConfig:
 
 celery_app = Celery('sync_poll_triggers', broker=RABBITMQ_BROKER_URL)
 celery_app.config_from_object(_celeryConfig())
+
+@worker_shutdown.connect
+def celery_shutdown():
+	close_connections()
 
 @celery_app.task()
 def trigger_poll(service_id, index):
