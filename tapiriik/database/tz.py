f5c74246b4924cd407deb4edb6c4ec2b88d3102a
Collin Fair <cpf@cpfx.ca>
2014-07-28 12:48:45 -0400

    Updated geonear query syntax for TZ resolution

--- tapiriik/database/tz.py
+++ tapiriik/database/tz.py
@@ -5,7 +5,7 @@ def TZLookup(lat, lng):
 	pt = [lng, lat]
 	res = tzdb.boundaries.find_one({"Boundary": {"$geoIntersects": {"$geometry": {"type":"Point", "coordinates": pt}}}}, {"TZID": True})
 	if not res:
-		res = tzdb.boundaries.find_one({"Boundary": SON([("$near", {"$geometry": {"type": "Point", "coordinates": pt}}), ("$maxDistance", 200000)])}, {"TZID": True})
+		res = tzdb.boundaries.find_one({"Boundary": {"$near": {"$geometry": {"type": "Point", "coordinates": pt}, "$maxDistance": 200000}}}, {"TZID": True})
 	res = res["TZID"] if res else None
 	if not res or res == "uninhabited":
 		res = round(lng / 15)
