95ceeb0af4e549e0d211b4e1ba6157d26ad5e44d
Collin <cpf@cpfx.ca>
2014-09-25 12:45:06 -0700

    Fix race between MQ and mongo setting QueuedAt

--- sync_scheduler.py
+++ sync_scheduler.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from pymongo.read_preferences import ReadPreference
 import kombu
 import time
+from tapiriik.settings import MONGO_FULL_WRITE_CONCERN
 
 Sync.InitializeWorkerBindings()
 
@@ -22,10 +23,10 @@ while True:
 				},
 				read_preference=ReadPreference.PRIMARY
 			)
-	scheduled_ids = set()
+	scheduled_ids = [x["_id"] for x in users]
+	db.users.update({"_id": {"$in": scheduled_ids}}, {"$set": {"QueuedAt": queueing_at}, "$unset": {"NextSynchronization": True}}, multi=True, w=MONGO_FULL_WRITE_CONCERN)
 	for user in users:
 		producer.publish({"user_id": str(user["_id"]), "queued_at": queueing_at.isoformat()}, routing_key=user["SynchronizationHostRestriction"] if "SynchronizationHostRestriction" in user and user["SynchronizationHostRestriction"] else "")
-		scheduled_ids.add(user["_id"])
 	print("Scheduled %d users at %s" % (len(scheduled_ids), datetime.utcnow()))
-	db.users.update({"_id": {"$in": list(scheduled_ids)}}, {"$set": {"QueuedAt": queueing_at}, "$unset": {"NextSynchronization": True}}, multi=True)
+	
 	time.sleep(1)
