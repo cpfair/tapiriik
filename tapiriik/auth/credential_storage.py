ce337b99757df6814bf12a52122fb2723f70e618
Collin Fair <cpf@cpfx.ca>
2014-10-13 16:24:27 -0700

    Cache credentials in memory to let the request finish other tasks without the private key

--- tapiriik/auth/credential_storage.py
+++ tapiriik/auth/credential_storage.py
@@ -3,6 +3,7 @@ from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA
 from Crypto import Random
 from tapiriik.settings import CREDENTIAL_STORAGE_PUBLIC_KEY, CREDENTIAL_STORAGE_PRIVATE_KEY
+import copy
 
 #### note about tapiriik and credential storage ####
 # Some services require a username and password for every action - so they need to be stored in recoverable form
@@ -17,12 +18,31 @@ class CredentialStore:
 
     def Encrypt(cred):
         data = CredentialStore._cipher.encrypt(cred.encode("UTF-8"))
-        return data
+
+        # We store the plaintext credential so that the web server can use it later during the same request.
+        # After the request terminates, it'll be GC'd more-or-less as quickly as the incoming request data itself.
+        return ShadowedCredential(data, cred)
 
     def Decrypt(data):
+        # Check if we encrypted the data in the same session, and use that instead (we might not have the private key).
+        if isinstance(data, ShadowedCredential):
+            return data.plaintext
+
         # I kind of doubt anyone could get away with a timing attack on the sycnhronization workers.
         # But, dear comment-reader, I'm sure you're now contemplating the possibilities...
         # So PKCS#1 OAEP it is.
         return CredentialStore._cipher.decrypt(data).decode("UTF-8")
+
+    def FlattenShadowedCredentials(auth_dict):
+        auth_dict = copy.deepcopy(auth_dict)
+        for k, v in auth_dict.items():
+            if isinstance(v, ShadowedCredential):
+                v = ShadowedCredential.ciphertext
+        return auth_dict
+
+class ShadowedCredential:
+    def __init__(self, ciphertext, plaintext):
+        self.ciphertext = ciphertext
+        self.plaintext = plaintext
 
 CredentialStore.Init()
