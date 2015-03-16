4c29d0418b2f45e37fa0de4dd855fcb109a078fc
Collin Fair <cpf@cpfx.ca>
2015-03-16 18:02:05 -0400

    Move webhook sync triggering out of the web worker

---
+++ sync_remote_triggers.py
@@ -0,0 +1,32 @@
+from tapiriik.database import db, close_connections
+from tapiriik.settings import RABBITMQ_BROKER_URL, MONGO_FULL_WRITE_CONCERN
+from datetime import datetime
+from celery import Celery
+from celery.signals import worker_shutdown
+
+class _celeryConfig:
+    CELERY_ROUTES = {
+        "sync_remote_triggers.trigger_remote": {"queue": "tapiriik-remote-trigger"}
+    }
+    CELERYD_CONCURRENCY = 1 # Otherwise the GC rate limiting breaks since file locking is per-process.
+    CELERYD_PREFETCH_MULTIPLIER = 1 # The message queue could use some exercise.
+
+celery_app = Celery('sync_remote_triggers', broker=RABBITMQ_BROKER_URL)
+celery_app.config_from_object(_celeryConfig())
+
+@worker_shutdown.connect
+def celery_shutdown():
+    close_connections()
+
+@celery_app.task()
+def trigger_remote(service_id, affected_connection_external_ids):
+    from tapiriik.auth import User
+    from tapiriik.services import Service
+    svc = Service.FromID(service_id)
+    db.connections.update({"Service": svc.ID, "ExternalID": {"$in": affected_connection_external_ids}}, {"$set":{"TriggerPartialSync": True, "TriggerPartialSyncTimestamp": datetime.utcnow()}}, multi=True, w=MONGO_FULL_WRITE_CONCERN)
+    affected_connection_ids = db.connections.find({"Service": svc.ID, "ExternalID": {"$in": affected_connection_external_ids}}, {"_id": 1})
+    affected_connection_ids = [x["_id"] for x in affected_connection_ids]
+    trigger_users_query = User.PaidUserMongoQuery()
+    trigger_users_query.update({"ConnectedServices.ID": {"$in": affected_connection_ids}})
+    trigger_users_query.update({"Config.suppress_auto_sync": {"$ne": True}})
+    db.users.update(trigger_users_query, {"$set": {"NextSynchronization": datetime.utcnow()}}, multi=True) # It would be nicer to use the Sync.Schedule... method, but I want to cleanly do this in bulk
--- tapiriik/web/views/sync.py
+++ tapiriik/web/views/sync.py
@@ -96,12 +96,7 @@ def sync_clear_errorgroup(req, service, group):
 def sync_trigger_partial_sync_callback(req, service):
     svc = Service.FromID(service)
     affected_connection_external_ids = svc.ExternalIDsForPartialSyncTrigger(req)
-    db.connections.update({"Service": svc.ID, "ExternalID": {"$in": affected_connection_external_ids}}, {"$set":{"TriggerPartialSync": True, "TriggerPartialSyncTimestamp": datetime.utcnow()}}, multi=True, w=MONGO_FULL_WRITE_CONCERN)
-    affected_connection_ids = db.connections.find({"Service": svc.ID, "ExternalID": {"$in": affected_connection_external_ids}}, {"_id": 1})
-    affected_connection_ids = [x["_id"] for x in affected_connection_ids]
-    trigger_users_query = User.PaidUserMongoQuery()
-    trigger_users_query.update({"ConnectedServices.ID": {"$in": affected_connection_ids}})
-    trigger_users_query.update({"Config.suppress_auto_sync": {"$ne": True}})
-    db.users.update(trigger_users_query, {"$set": {"NextSynchronization": datetime.utcnow()}}, multi=True) # It would be nicer to use the Sync.Schedule... method, but I want to cleanly do this in bulk
+
+    trigger_remote.apply_async(args=[service, affected_connection_external_ids])
     return HttpResponse(status=204)
 
