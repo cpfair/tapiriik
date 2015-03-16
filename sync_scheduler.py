37de63e9ff5ce1025aaecd0d7ae09d8b0ec29f40
Collin Fair <cpf@cpfx.ca>
2015-03-16 19:01:50 -0400

    More scheduler debug output

--- sync_scheduler.py
+++ sync_scheduler.py
@@ -27,7 +27,9 @@ while True:
                 read_preference=ReadPreference.PRIMARY
             ))
     scheduled_ids = [x["_id"] for x in users]
+    print("Found %d users at %s" % (len(scheduled_ids), datetime.utcnow()))
     db.users.update({"_id": {"$in": scheduled_ids}}, {"$set": {"QueuedAt": queueing_at, "QueuedGeneration": generation}, "$unset": {"NextSynchronization": True}}, multi=True, w=MONGO_FULL_WRITE_CONCERN)
+    print("Marked %d users as queued at %s" % (len(scheduled_ids), datetime.utcnow()))
     for user in users:
         producer.publish({"user_id": str(user["_id"]), "generation": generation}, routing_key=user["SynchronizationHostRestriction"] if "SynchronizationHostRestriction" in user and user["SynchronizationHostRestriction"] else "")
     print("Scheduled %d users at %s" % (len(scheduled_ids), datetime.utcnow()))
