a3623b069aa50bf7c68cc56aaa25a10465c4873d
Collin Fair <cpf@cpfx.ca>
2015-04-25 20:49:34 -0400

    Reduce sync log retention

--- tapiriik/sync/sync.py
+++ tapiriik/sync/sync.py
@@ -229,7 +229,7 @@ class SynchronizationTask:
         db.users.update({"_id": self.user["_id"]}, {"$set": {"SynchronizationProgress": progress, "SynchronizationStep": step}})
 
     def _initializeUserLogging(self):
-        self._logging_file_handler = logging.handlers.RotatingFileHandler(USER_SYNC_LOGS + str(self.user["_id"]) + ".log", maxBytes=0, backupCount=10, encoding="utf-8")
+        self._logging_file_handler = logging.handlers.RotatingFileHandler(USER_SYNC_LOGS + str(self.user["_id"]) + ".log", maxBytes=0, backupCount=5, encoding="utf-8")
         self._logging_file_handler.setFormatter(logging.Formatter(self._logFormat, self._logDateFormat))
         self._logging_file_handler.doRollover()
         _global_logger.addHandler(self._logging_file_handler)
