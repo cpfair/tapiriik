7faa26bb3e4db61386d5d47ec838f981b42df051
Collin Fair <cpf@cpfx.ca>
2015-04-26 11:36:50 -0400

    Stop performing un-indexed lookups against the sync_workers collection (also fixes glitches if hostname changes at runtime) Also, attempt to bail if server disk is readonly before adding an item in sync_workers

--- sync_worker.py
+++ sync_worker.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 # I'm trying to track down where some missing seconds are going in the sync process
 # Will grep these out of the log at some later date
@@ -10,6 +10,7 @@ worker_message("booting")
 from tapiriik.requests_lib import patch_requests_with_default_timeout, patch_requests_source_address
 from tapiriik import settings
 from tapiriik.database import db, close_connections
+from pymongo import ReturnDocument
 import sys
 import subprocess
 import socket
@@ -21,11 +22,37 @@ WorkerVersion = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess
 os.chdir(oldCwd)
 
 def sync_heartbeat(state, user=None):
-    db.sync_workers.update({"Process": os.getpid(), "Host": socket.gethostname()}, {"$set": {"Heartbeat": datetime.utcnow(), "State": state, "User": user}})
+    db.sync_workers.update({"_id": heartbeat_rec_id}, {"$set": {"Heartbeat": datetime.utcnow(), "State": state, "User": user}})
 
 worker_message("initialized")
-db.sync_workers.update({"Process": os.getpid(), "Host": socket.gethostname()}, {"Process": os.getpid(), "Heartbeat": datetime.utcnow(), "Startup":  datetime.utcnow(),  "Version": WorkerVersion, "Host": socket.gethostname(), "Index": settings.WORKER_INDEX, "State": "startup"}, upsert=True)
+
+# Moved this flush before the sync_workers upsert for a rather convoluted reason:
+# Some of the sync servers were encountering filesystem corruption, causing the FS to be remounted as read-only.
+# Then, when a sync worker would start, it would insert a record in sync_workers then immediately die upon calling flush - since output is piped to a log file on the read-only FS.
+# Supervisor would dutifully restart the worker again and again, causing sync_workers to quickly fill up.
+# ...which is a problem, since it doesn't have indexes on Process or Host - what later lookups were based on. So, the database would be brought to a near standstill.
+# Theoretically, the watchdog would clean up these records soon enough - but since it too logs to a file, it would crash removing only a few stranded records
+# By flushing the logs before we insert, it should crash before filling that collection up.
+# (plus, we no longer query with Process/Host in sync_hearbeat)
+
 sys.stdout.flush()
+heartbeat_rec = db.sync_workers.find_one_and_update(
+	{
+		"Process": os.getpid(),
+		"Host": socket.gethostname()
+	}, { 
+		"$set": {
+			"Process": os.getpid(),
+			"Host": socket.gethostname(),
+			"Heartbeat": datetime.utcnow(),
+			"Startup":  datetime.utcnow(),
+			"Version": WorkerVersion,
+			"Index": settings.WORKER_INDEX,
+			"State": "startup"
+		}
+	}, upsert=True,
+	return_document=ReturnDocument.AFTER)
+heartbeat_rec_id = heartbeat_rec["_id"]
 
 patch_requests_with_default_timeout(timeout=60)
 
@@ -48,7 +75,7 @@ worker_message("ready")
 Sync.PerformGlobalSync(heartbeat_callback=sync_heartbeat, version=WorkerVersion, max_users=RecycleInterval)
 
 worker_message("shutting down cleanly")
-db.sync_workers.remove({"Process": os.getpid(), "Host": socket.gethostname()})
+db.sync_workers.remove({"_id": heartbeat_rec_id})
 close_connections()
 worker_message("shut down")
 sys.stdout.flush()
