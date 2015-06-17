1b5143d20eef4fe17794b03f82f6e02d351dba73
Collin Fair <cpf@cpfx.ca>
2015-06-16 22:38:21 -0400

    June 21 almost snuck up on me again

--- tapiriik/settings.py
+++ tapiriik/settings.py
@@ -219,7 +219,6 @@ PAYMENT_CURRENCY = "USD"
 
 # Celebration mode config
 # Because why not, I'm waiting for my account to get to the front of the sync queue.
-# Direct requests to contact@tapiriik.com
 
 CELEBRATION_MODES = {
     (
@@ -228,52 +227,8 @@ CELEBRATION_MODES = {
     ): {
         "Logo": "tapiriik-inuktitut.png",
         "Subtitle": "National Aboriginal Day",
-        "TitleText": "ᖃᐃᒋᑦ, ᐊᖅᑲᖅᑕ ᐅᖃᐅᓰᑦ ᐃᓚᑦᓯᓪᓗᒋᑦ ᐃᖕᒥᓐᓂᖕᓄᑦ ᑐᑭᓯᓯᓐᓈᔪᖕᓇᖅᑎᓪᓚᒋᑦ (Genesis 11:7)" # Fitting
-    },
-    (
-        datetime(day=28, month=6, year=2014, hour=0, minute=0), # Playing it safe
-        datetime(day=29, month=6, year=2014, hour=23, minute=59)
-    ): {
-        "Logo": "tapiriik-arabic.png", # تبريك
-        "Subtitle": "Ramadan Mubarak! !رمضان مبارك", # I'm trusting you, magic RTL support!
-        "TitleText": "(42:43) وَلَمَن صَبَرَ وَغَفَرَ إِنَّ ذَٰلِكَ لَمِنْ عَزْمِ الْأُمُورِ",
-        "BodyCSSClasses": "night crescent_moon starfield"
-    },
-    (
-        datetime(day=28, month=7, year=2014, hour=0, minute=0),
-        datetime(day=28, month=7, year=2014, hour=23, minute=59)
-    ): {
-        "Logo": "tapiriik-arabic.png",
-        "Subtitle": "Eid Mubarak! !عيد مبارك‎",
-        "TitleText": "(42:43) وَلَمَن صَبَرَ وَغَفَرَ إِنَّ ذَٰلِكَ لَمِنْ عَزْمِ الْأُمُورِ",
-        "BodyCSSClasses": "night crescent_moon starfield"
-    },
-    (
-        datetime(day=23, month=10, year=2014, hour=0, minute=0),
-        datetime(day=23, month=10, year=2014, hour=23, minute=59)
-    ): {
-        "Logo": "tapiriik-hindi.png", # तआपेरीक
-        "Subtitle": "Shubh Deepawali! शुभ दीपावली!",
-        "TitleText": "ॐ शान्ति शान्ति शान्ति",
-        "BodyCSSClasses": "night full_moon starfield"
-    },
-    (
-        datetime(day=6, month=11, year=2014, hour=0, minute=0),
-        datetime(day=6, month=11, year=2014, hour=23, minute=59)
-    ): {
-        "Logo": "tapiriik-punjabi.png", # ਤਾਪਿਰੀਕ
-        "Subtitle": "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ!",
-        "TitleText": "ਆਪੁ ਗਇਆ ਸੁਖੁ ਪਾਇਆ ਮਿਲਿ ਸਲਲੈ ਸਲਲ ਸਮਾਇ"
-    },
-    (
-        datetime(day=24, month=9, year=2014, hour=18, minute=0),
-        datetime(day=26, month=9, year=2014, hour=18, minute=0)
-    ): {
-        "Logo": "tapiriik-hebrew.png", # טפּרכּ
-        "Subtitle": "!לְשָׁנָה טוֹבָה",
-        "TitleText": "הָבָה, נֵרְדָה, וְנָבְלָה שָׁם, שְׂפָתָם--אֲשֶׁר לֹא יִשְׁמְעוּ, אִישׁ שְׂפַת רֵעֵהוּ. (Genesis 11:7)",
-        "BodyCSSClasses": "night crescent_moon starfield"
-    },
+        "TitleText": "Our Home on Native Land"
+    }
 }
 
 
