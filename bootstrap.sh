9a43a86f8b883f2534aff9af1ce87ee109577b4f
Ryan Campbell <campbellr@gmail.com>
2015-07-23 00:17:37 -0600

    vagrant: use mongodb 2.6.9
    This fixes #156

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 # Install system requirements
+
+# mongodb multipolygon geojson support needs at least mongodb 2.6. trusty has 2.4 by default
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+
 sudo apt-get update
-sudo apt-get install -y python3-pip libxml2-dev libxslt-dev zlib1g-dev mongodb git redis-server rabbitmq-server
+sudo apt-get install -y python3-pip libxml2-dev libxslt-dev zlib1g-dev git redis-server rabbitmq-server mongodb-org=2.6.9
 
 # Fix pip
 pip3 install --upgrade pip
