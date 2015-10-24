ef5a5974cb7ad1d9eff859a44f438518e894ebe0
Collin Fair <cpf@cpfx.ca>
2015-10-23 20:54:07 -0400

    Don't require secondaries to be online in the scheduler - oops.

--- sync_scheduler.py
+++ sync_scheduler.py
@@ -1,12 +1,10 @@
 from tapiriik.database import db
-from tapiriik.messagequeue import mq
 from tapiriik.sync import Sync
 from datetime import datetime
 from pymongo.read_preferences import ReadPreference
 import kombu
 import time
 import uuid
-from tapiriik.settings import MONGO_FULL_WRITE_CONCERN
 
 Sync.InitializeWorkerBindings()
 
@@ -27,7 +25,7 @@ while True:
             ))
     scheduled_ids = [x["_id"] for x in users]
     print("Found %d users at %s" % (len(scheduled_ids), datetime.utcnow()))
-    db.users.update({"_id": {"$in": scheduled_ids}}, {"$set": {"QueuedAt": queueing_at, "QueuedGeneration": generation}, "$unset": {"NextSynchronization": True}}, multi=True, w=MONGO_FULL_WRITE_CONCERN)
+    db.users.update({"_id": {"$in": scheduled_ids}}, {"$set": {"QueuedAt": queueing_at, "QueuedGeneration": generation}, "$unset": {"NextSynchronization": True}}, multi=True)
     print("Marked %d users as queued at %s" % (len(scheduled_ids), datetime.utcnow()))
     for user in users:
         producer.publish({"user_id": str(user["_id"]), "generation": generation}, routing_key=user["SynchronizationHostRestriction"] if "SynchronizationHostRestriction" in user and user["SynchronizationHostRestriction"] else "")
