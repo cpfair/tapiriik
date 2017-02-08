1acc9c1dc0f9366d8a7091a2a2b7bdc1bf21fb64
Collin Fair <cpf@cpfx.ca>
2017-02-08 08:27:23 -0500

    Make Strength Training activity type value consistent.

--- tapiriik/services/interchange.py
+++ tapiriik/services/interchange.py
@@ -22,7 +22,7 @@ class ActivityType:  # taken from RK API docs. The text values have no meaning e
     Gym = "Gym"
     Climbing = "Climbing"
     RollerSkiing = "RollerSkiing"
-    StrengthTraining = "Strength Training"
+    StrengthTraining = "StrengthTraining"
     Other = "Other"
 
     def List():
