e3107fa1fbf4451ee52ae12fabe230f3f16d43fe
Collin <cpf@cpfx.ca>
2017-02-20 14:18:39 -0500

    Update privacy page for TrainAsONE and pulsstory

--- tapiriik/web/views/privacy.py
+++ tapiriik/web/views/privacy.py
@@ -27,6 +27,8 @@ def privacy(request):
     services["trainerroad"].update({"email": OPTIN, "password": OPTIN, "tokens": NO, "metadata": YES, "data":NO})
     services["smashrun"].update({"email": NO, "password": NO, "tokens": YES, "metadata": YES, "data":NO})
     services["beginnertriathlete"].update({"email": OPTIN, "password": OPTIN, "tokens": NO, "metadata": YES, "data": NO})
+    services["trainasone"].update({"email": NO, "password": NO, "tokens": YES, "metadata": YES, "data":NO})
+    services["pulsstory"].update({"email": NO, "password": NO, "tokens": YES, "metadata": YES, "data":NO})
 
     for svc_id in SOFT_LAUNCH_SERVICES:
         if svc_id in services:
