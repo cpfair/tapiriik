c673357b9cdc7c67aa3fe10952d02a03c98665b1
Anton Ashmarin <aashmarin@gmail.com>
2018-02-02 14:06:21 +0300

    Revert "Delete activity done,"
    This reverts commit 75fb6eba93afb7b5b8f1a30bd1741322b6f10d05.

--- tapiriik/services/Aerobia/aerobia.py
+++ tapiriik/services/Aerobia/aerobia.py
@@ -1,16 +1,11 @@
-# Synchronisation module for aerobia.ru
-# (c) 2018 Anton Ashmarin, aashmarin@gmail.com
 from tapiriik.services.service_base import ServiceAuthenticationType, ServiceBase
 from tapiriik.services.service_record import ServiceRecord
-from tapiriik.services.interchange import UploadedActivity, ActivityType, ActivityStatistic, ActivityStatisticUnit, Waypoint, Location, Lap
 from tapiriik.services.api import APIException, UserException, UserExceptionType, APIExcludeActivity
-from tapiriik.services.tcx import TCXIO
 from tapiriik.services.sessioncache import SessionCache
 
 import requests
 import logging
 import re
-import os
 
 from datetime import datetime, timedelta
 
@@ -23,40 +18,7 @@ class AerobiaService(ServiceBase):
     AuthenticationType = ServiceAuthenticationType.UsernamePassword
     RequiresExtendedAuthorizationDetails = True
 
-    #common -> aerobia
-    _activityMappings = {
-        ActivityType.Running : 2,
-        ActivityType.Cycling : 1,
-        #ActivityType.MountainBiking,
-        ActivityType.Walking : 19,
-        #ActivityType.Hiking,
-        #ActivityType.DownhillSkiing,
-        ActivityType.CrossCountrySkiing : 3,
-        #ActivityType.Skating,
-        ActivityType.Swimming : 21,
-        #ActivityType.Rowing,
-        #ActivityType.Elliptical,
-        #ActivityType.Gym,
-        #ActivityType.Climbing,
-        #ActivityType.StrengthTraining,
-        ActivityType.Other : 68
-    }
-
-    #todo fill mappings
-    _reverseActivityMappings = {
-        1 : ActivityType.Cycling,
-        2 : ActivityType.Running,
-
-        68 : ActivityType.Other
-    }
-
-    SupportedActivities = list(_activityMappings.keys())
-
-    SupportsHR = SupportsCadence = True
-
-    SupportsActivityDeletion = True
-
-    _sessionCache = SessionCache("aerobia", lifetime=timedelta(minutes=120), freshen_on_get=False)
+    _sessionCache = SessionCache("aerobia", lifetime=timedelta(minutes=120), freshen_on_get=True)
 
     _urlRoot = "http://aerobia.ru/"
     _loginUrlRoot = _urlRoot + "users/sign_in"
@@ -64,10 +26,6 @@ class AerobiaService(ServiceBase):
     def _get_session(self, record=None, username=None, password=None, skip_cache=False):
         from tapiriik.auth.credential_storage import CredentialStore
         from tapiriik.requests_lib import patch_requests_user_agent
-
-        # Without user-agent patch aerobia requests doesn't work
-        patch_requests_user_agent('Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko')
-
         cached = self._sessionCache.Get(record.ExternalID if record else username)
         if cached and not skip_cache:
             logger.debug("Using cached credential")
@@ -78,33 +36,35 @@ class AerobiaService(ServiceBase):
             username = CredentialStore.Decrypt(record.ExtendedAuthorization["Email"])
 
         session = requests.Session()
+        # Without user-agent patch aerobia requests doesn't work   
+        patch_requests_user_agent('Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko')
 
-        request_parameters = {"user[email]": username, "user[password]": password}
-        res = session.post(self._loginUrlRoot, data=request_parameters)
+        requestParameters = {"user[email]": username,  "user[password]": password}
+        res = session.post(self._loginUrlRoot, data=requestParameters)
 
         if res.status_code >= 500 and res.status_code < 600:
             raise APIException("Login exception %s - %s" % (res.status_code, res.text), user_exception=UserException(UserExceptionType.Authorization))
 
         # Userid is needed for urls
-        id_match = re.search(r"users/(\d+)/workouts", res.text)
-        if not id_match:
+        idMatch = re.search(r"users/(\d+)/workouts", res.text)
+        if not idMatch:
             raise APIException("Invalid login", block=True, user_exception=UserException(UserExceptionType.Authorization, intervention_required=True))
-        
-        session.user_id = id_match.group(1)
-        
+        else:
+            session.user_id = idMatch.group(1)
+
         # Token is passed with GET queries as a parameter
-        token_match = re.search(r"meta content=\"(.+)\" name=\"csrf-token", res.text)
-        if not token_match:
+        tokenMatch = re.search(r"meta content=\"(.+)\" name=\"csrf-token", res.text)
+        if not tokenMatch:
             raise APIException("Invalid login", block=True, user_exception=UserException(UserExceptionType.Authorization, intervention_required=True))
-        
-        session.authenticity_token = token_match.group(1)
+        else:
+            session.authenticity_token = tokenMatch.group(1)
 
         self._sessionCache.Set(record.ExternalID if record else username, session)
 
         return session
 
     def _with_auth(self, session, params={}):
-        # For whatever reason the authenticity_token needs to be a parameter
+        # For whatever reason the authenticity_token needs to be a GET parameter :(
         params.update({"\"authenticity_token\"": session.authenticity_token})
         return params
 
@@ -115,63 +75,17 @@ class AerobiaService(ServiceBase):
         return (session.user_id, {}, {"Email": CredentialStore.Encrypt(username), "Password": CredentialStore.Encrypt(password)})
 
     def DownloadActivityList(self, serviceRecord, exhaustive_start_date=None):
-        session = self._get_session(serviceRecord)
-
-        activities = []
-        exclusions = []
-        #todo get date from the first request to get guaranteed last training
-        date = datetime.now()
-        date_format = "%Y-%m-01"
-
-        while True:
-            list_params = {"month": date.strftime(date_format)}
-            dairy_data = session.get(self._urlRoot + "users/%s/workouts" %serviceRecord.ExternalID, params=list_params)
-
-            act_data_list = self._extract_activities(dairy_data.text)
-            for act_data in act_data_list:
-                activity = self._create_activity(act_data)
-                #todo need to exclude repeated data due to calendar output overlap 
-                activities.append(activity)
-            #todo will stop if user didn't do excercises during a particular month
-            if len(act_data_list) == 0 or not exhaustive_start_date:
-                break
-
-            date = date - timedelta(days=1)
-            date = date.replace(days=1)
-
-        return activities, exclusions
-
-    def _extract_activities(self, html):
-        #todo parse html to get array of json training objects
-        return []
-
-    def _create_activity(self, activity_data):
-        activity = UploadedActivity()
-        #todo fill fields from activity_data
-        
-        activity.CalculateUID()
-        return activity
+        pass
 
     def DownloadActivity(self, serviceRecord, activity):
-        session = self._get_session(serviceRecord)
-        tcx_data = session.get(self._urlRoot + "export/workouts/%d/tcx" %activity.ServiceData["ActivityID"])
-        return TCXIO.Parse(tcx_data, activity)
+        pass
 
-    # todo Should return an uploadId for storage and potential use in DeleteActivity
+    # Should return an uploadId for storage and potential use in DeleteActivity
     def UploadActivity(self, serviceRecord, activity):
-        session = self._get_session(serviceRecord)
-        tcx_data = TCXIO.Dump(activity)
-        file = {"workout_file[file][]": ("tap-sync-" + str(os.getpid()) + "-" + activity.UID + ".tcx", tcx_data)}
-        #todo why session dont contains token?
-        req = session.post(self._urlRoot + "import/files", params=self._with_auth(session), files=file)
         pass
 
     def DeleteActivity(self, serviceRecord, uploadId):
-        session = self._get_session(serviceRecord)
-
-        delete_parameters = {"_method" : "delete"}
-        delete_parameters = self._with_auth(session, delete_parameters)
-        session.post(self._urlRoot + "workouts/%d" %uploadId, data=delete_parameters)
+        pass
 
     def DeleteCachedData(self, serviceRecord):
         pass  # No cached data...
