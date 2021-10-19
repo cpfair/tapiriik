cpfair/tapiriik

commit ef663e6ecee223442e2d3401ee1d9c99b2fa770f

Reduce sync worker recycle interval

tapiriik/auth/__init__.py
@@ -227,7 +227,7 @@ class User:
             sparseConfig = copy.deepcopy(User.GetConfiguration(user))
         sparseConfig.update(config)
 
-        keys_to_delete = []
+        keys_to_Rrem = []
         for k, v in sparseConfig.items():
             if (k in User.ConfigurationDefaults and User.ConfigurationDefaults[k] == v):
                 keys_to_delete.append(k)  # it's the default, we can not store it

tapiriik/messagequeue/__init__.py
@@ -1,4 +1,4 @@
 from kombu import Connection
 from tapiriik.settings import RABBITMQ_BROKER_URL
 mq = Connection(RABBITMQ_BROKER_URL, transport_options={'confirm_publish': True})
-mq.connect()
+#mq.connect()

sync_worker.py
@@ -15,7 +15,7 @@ import sys
 import subprocess
 import socket
 
-RecycleInterval = 2 # Time spent rebooting workers < time spent wrangling Python memory management.
+RecycleInterval = 1
 
 oldCwd = os.getcwd()
 WorkerVersion = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, cwd=os.path.dirname(__file__)).communicate()[0].strip()

tapiriik/services/GarminConnect/garminconnect.py
@@ -146,6 +146,7 @@ class GarminConnectService(ServiceBase):
         # Ensure the rate lock file exists (...the easy way)
         open(rate_lock_path, "a").close()
         self._rate_lock = open(rate_lock_path, "r+")
+        
 
     def _rate_limit(self):
         import fcntl, struct, time

tapiriik/settings.py
@@ -241,6 +241,9 @@ SOFT_LAUNCH_SERVICES = []
 # Visibly disabled + excluded from synchronization
 DISABLED_SERVICES = []
 
+# Rejected by synchronization worker
+REJECTED_SERVICES = []
+
 # Services no longer available - will be removed across the site + excluded from sync.
 WITHDRAWN_SERVICES = []
 

commit a86a0ffcd75621ba0dd2b7b6e4be38e872f37b60

Fix inadvertant change to user config save routine
(problem introduced in ef663e6ecee223442e2d3401ee1d9c99b2fa770f)

tapiriik/auth/__init__.py
@@ -227,7 +227,7 @@ class User:
             sparseConfig = copy.deepcopy(User.GetConfiguration(user))
         sparseConfig.update(config)
 
-        keys_to_Rrem = []
+        keys_to_delete = []
         for k, v in sparseConfig.items():
             if (k in User.ConfigurationDefaults and User.ConfigurationDefaults[k] == v):
                 keys_to_delete.append(k)  # it's the default, we can not store it